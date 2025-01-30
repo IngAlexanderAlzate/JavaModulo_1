e8c39b6630c714b4fa98cdd32d2744829ae3b208
Alexander_Alzate <rubio017@gmail.com>
2025-01-29 21:59:04 -0500

    Funcion Repetitivas

---
+++ Java/Repetitivas/For/SumaNumeros.java
@@ -0,0 +1,15 @@
+package Java.Repetitivas.For;
+
+import java.util.function.BiFunction;
+
+public class SumaNumeros {
+    public static void main(String[]args){
+        // Usamos una Lambda para representar una función de suma
+        BiFunction<Integer, Integer, Integer> suma =  (a, b) -> a + b;
+
+        // Llamada a la función usando los parámetros directamente
+        int resultado = suma.apply(5,3);
+        System.out.println("Resultado: " + resultado);
+
+    }
+}
